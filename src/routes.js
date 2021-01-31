ee00e7ca7b4fdf1c7753e212301f3ec021ffa6b6
Yogesh <surapaneniyogesh11@gmail.com>
2021-01-31 15:48:20 +0530

    :sparkles: Initialized Timelines

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import Home from "./routes/Home.svelte";
 import Working from "./routes/Working.svelte";
+import TimeLine from "./routes/TimeLine.svelte"
 
 export default {
-  '/': Home,
-  '/assignments': Working,
-  '/timetables': Working,
-  '/resources': Working,
+    '/': Home,
+    '/assignments': Working,
+    '/timetables': TimeLine,
+    '/resources': Working,
 }
